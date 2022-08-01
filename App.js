497e55e7aeca1114eecc386e9df8b98d4cf9140b
calvin chung <calvin.w.chung@gmail.com>
2022-08-01 11:36:51 -0400

    fixing tickle spelling error

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Ticke Ball</Text>
+      <Text>Tickle Ball</Text>
       <StatusBar style="auto" />
     </View>
   );
