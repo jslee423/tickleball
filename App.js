3874e4b44dfe0a1f42277b505ce9e21298513520
jslee423 <44250376+jslee423@users.noreply.github.com>
2022-08-02 16:59:58 -0400

    add basic tab navigation and fix asset images paths (#5)

--- App.js
+++ App.js
@@ -1,24 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import HomePage from './screens/HomePage';
+import TabNavigator from './navigation/TabNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
       <NavigationContainer>
-        <HomePage />
+        <TabNavigator/>
+        <StatusBar style="auto"/>
       </NavigationContainer>
-      <StatusBar style="auto" />
-    </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
