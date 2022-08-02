ed3df6fe8ef1f0c6b7d4e1e3c4dac59bcfcabc79
calvin chung <calvin.w.chung@gmail.com>
2022-08-02 16:15:56 -0400

    working on homepage and added navi to the app.js

---
+++ screens/HomePage.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Text, StyleSheet } from 'react-native';
+import PB1 from '../assets/images/pb1.png';
+import PB2 from '../assets/images/pb2.png';
+import PB3 from '../assets/images/pb3.png';
+import { Image } from "react-native"; 
+
+const HomePage = () => {
+  return (
+    <>
+      <Text style={styles.text}></Text>
+      <Image 
+        source={ PB3 }
+        style={styles.imageHeader}
+      />
+      <Image 
+        source={ PB1 }
+        style={styles.image}
+      />
+      <Text>Find out what your pickleball rating is!</Text>
+      <Image 
+        source={ PB2 }
+        style={styles.image}
+      />
+      <Text>Shop the paddles that we recommend!</Text>
+    </>
+  )
+};
+
+const styles = StyleSheet.create({
+  image: {
+    resizeMode: 'contain',
+    height: 200,
+    borderRadius: 50
+  },
+  imageHeader: {
+    resizeMode: 'contain',
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 15
+  },
+
+  text: {
+    fontSize: 30,
+    marginBottom: 40
+    
+  }
+})
+
+export default HomePage;
