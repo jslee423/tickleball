65d4eff50eeb6d3835bbf4a65d7f75752680264f
Calvin Chung <99142060+calvinwchung@users.noreply.github.com>
2022-08-02 16:18:45 -0400

    working on homepage and added navi to the app.js (#4)

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import HomePage from './screens/HomePage';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Tickle Ball jason!</Text>
+      <NavigationContainer>
+        <HomePage />
+      </NavigationContainer>
       <StatusBar style="auto" />
     </View>
   );
