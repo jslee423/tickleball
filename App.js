c1937060a9c6f3a49d58606ebda8e6f2f15f3fb4
Andrew Kim <ajkim1018@gmail.com>
2022-08-02 12:10:24 -0400

    ak branch test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Tickle Ball</Text>
+      <Text>Tickle Ball Branch Test Ak</Text>
       <StatusBar style="auto" />
     </View>
   );
