6a44d12755e519614eead72057fc96512b463b70
jslee423 <44250376+jslee423@users.noreply.github.com>
2022-08-03 10:01:29 -0400

    add icons to bottom tab (#6)

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomePage from "../screens/HomePage";
 import { Text, View } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 const TabNavigator = () => {
     const ProfileScreen = () => {
@@ -14,10 +15,30 @@ const TabNavigator = () => {
     const Tab = createBottomTabNavigator();
 
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+    
+                if (route.name === 'Home') {
+                    iconName = focused ? 'home' : 'home';
+                } else if (route.name === 'Settings') {
+                    iconName = focused ? 'sliders' : 'sliders';
+                } else if (route.name === 'Profile') {
+                    iconName = focused ? 'user-circle-o' : 'user-circle-o';
+                }
+
+                // You can return any component that you like here!
+                // return <Ionicons name={iconName} size={size} color={color} />;
+                return <FontAwesome name={iconName} size={size} color={color} />;
+                },
+                tabBarActiveTintColor: 'tomato',
+                tabBarInactiveTintColor: 'gray',
+            })}
+        >
             <Tab.Screen name='Home' component={HomePage} />
-            <Tab.Screen name='Profile' component={ProfileScreen} />
             <Tab.Screen name='Settings' component={HomePage} />
+            <Tab.Screen name='Profile' component={ProfileScreen} />
         </Tab.Navigator>
     );
 };
