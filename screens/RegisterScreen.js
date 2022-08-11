27318da9bccc2e842f840d6dfafcc04cb53e0661
jslee423 <44250376+jslee423@users.noreply.github.com>
2022-08-11 17:18:15 -0400

    add input validation, ad TextInput component and remove duplicate files (#17)

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,22 +1,35 @@
 import { useState } from "react";
-import { Text, View, Image, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import { Text, View, Image, StyleSheet, TouchableOpacity } from "react-native";
 import Logo from '../assets/images/doublesPaddle.png';
+import TextInputField from "../components/TextInputField";
+import { nameValidator, emailValidator, passwordValidator } from "../utils/InputValidators";
 
 const RegisterScreen = ({navigation}) => {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [password, setPassword] = useState('');
-    const [email, setEmail] = useState('');
+    const [firstName, setFirstName] = useState({value: '', error: ''});
+    const [lastName, setLastName] = useState({value: '', error: ''});
+    const [password, setPassword] = useState({value: '', error: ''});
+    const [email, setEmail] = useState({value: '', error: ''});
 
     const handleRegister = () => {
         const userInfo = {
-            firstName,
-            lastName,
-            password,
-            email
+            firstName: firstName.value,
+            lastName: lastName.value,
+            password: password.value,
+            email: email.value
         };
         console.log(JSON.stringify(userInfo));
 
+        const firstNameError = nameValidator(firstName.value);
+        const lastNameError = nameValidator(lastName.value);
+        const emailError = emailValidator(email.value);
+        const passwordError = passwordValidator(password.value);
+        if (firstNameError || lastNameError || emailError || passwordError) {
+            setFirstName({...firstName, error: firstNameError});
+            setLastName({...lastName, error: lastNameError});
+            setEmail({...email, error: emailError});
+            setPassword({...password, error: passwordError})
+            return;
+        }
         navigation.navigate('registerSuccess')
     };
 
@@ -26,46 +39,34 @@ const RegisterScreen = ({navigation}) => {
             <View style={styles.header}>
                 <Text style={styles.headerText}>Create Account</Text>
             </View>
-            <View style={styles.inputView}>
-                <TextInput
-                    style={styles.input}
-                    placeholder='first name'
-                    placeholderTextColor='#ddddff'
-                    autoCapitalize='none'
-                    onChangeText={(name) =>setFirstName(name)}
-                    value={firstName}
-                />
-            </View>
-            <View style={styles.inputView}>
-                <TextInput
-                    style={styles.input}
-                    placeholder='last name'
-                    placeholderTextColor='#ddddff'
-                    autoCapitalize='none'
-                    onChangeText={(name) =>setLastName(name)}
-                    value={lastName}
-                />
-            </View>
-            <View style={styles.inputView}>
-                <TextInput
-                    style={styles.input}
-                    placeholder='email'
-                    placeholderTextColor='#ddddff'
-                    autoCapitalize='none'
-                    onChangeText={(email) =>setEmail(email)}
-                    value={email}
-                />
-            </View>
-            <View style={styles.inputView}>
-                <TextInput
-                    style={styles.input}
-                    placeholder='password'
-                    placeholderTextColor='#ddddff'
-                    autoCapitalize='none'
-                    onChangeText={(password) =>setPassword(password)}
-                    value={password}
-                />
-            </View>
+            <TextInputField
+                placeholder='first name'
+                onChangeText={(name) => setFirstName({value: name, error: ''})}
+                value={firstName.value}
+                error={!!firstName.error}
+                errorText={firstName.error}
+            />
+            <TextInputField
+                placeholder='last name'
+                onChangeText={(name) =>setLastName({value: name, error: ''})}
+                value={lastName.value}
+                error={!!lastName.error}
+                errorText={lastName.error}
+            />
+            <TextInputField
+                placeholder='email'
+                onChangeText={(email) =>setEmail({value: email, error: ''})}
+                value={email.value}
+                error={!!email.error}
+                errorText={email.error}
+            />
+            <TextInputField
+                placeholder='password'
+                onChangeText={(password) =>setPassword({value: password, error: ''})}
+                value={password.value}
+                error={!!password.error}
+                errorText={password.error}
+            />
             <TouchableOpacity
                 style={styles.signUpBtn}
                 onPress={() => handleRegister()}
@@ -101,20 +102,6 @@ const styles = StyleSheet.create({
         height: 200,
         width: 200,
         marginBottom: 10
-    },
-    inputView: {
-        backgroundColor: '#fff',
-        borderStyle: 'solid',
-        borderColor: '#04f167',
-        borderWidth: 1,
-        borderRadius: 10,
-        width: '70%',
-        height: 40,
-        marginBottom: 20,
-        alignItems: 'center'
-    },
-    input: {
-        flex: 1
     },
     signUpBtn: {
         width: '60%',
